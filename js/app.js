c48f75c1b3f6a816e9a13a100ca031aa7d1e06d1
Samantha Canedy <samcanedy@gmail.com>
2017-10-07 12:11:43 -0500

    Update app.js
    Moved Flickr API into global scope

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 // and third-party API.
 // The View is the HTML file.
 
+var googleMap;
+var infoWindow;
+var markers = [];
+var viewModel;
+
 function Landmarks(data, position) {
 	this.name = data.name;
 	this.latlngLoc = data.latlngLoc;
@@ -19,7 +24,7 @@ function Landmarks(data, position) {
 		populateInfoWindow(this);
 		toggleBounce(this);
 		bounceTimer(this);
-		// loadFlickr(this.title);
+		loadFlickr(this.title);
 		// console.log(this);
 	});
 }
@@ -51,63 +56,12 @@ function bounceTimer (marker) {
 	}, 1400);
 }
 
-// function loadFlickr (landmarks) {
-// 	var $flickrElem = $('#flickr-images');
+function loadFlickr (location) {
+	var $flickrElem = $('#flickr-images');
 
-// 	// this.landmarkPictures = ko.observableArray([]);
-
-// 	// Clear out old data before new request
-// 	$flickrElem.text('');
-// 	landmarkPictures([]);
-
-// 	var key = '88e693240daad97978bc4d93c370fd18';
-// 	var secret = '78ab07255ac49f98';
-
-// 	var flickrRequestTimeout = setInterval(function() {
-// 		$flickrElem.text('Failed to load Flickr Images');
-// 	}, 9000);
-
-// 	var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
-
-// 	$.getJSON(flickrURl, function (data) {
-// 		console.log(data);
-// 		var images = data.photos.photo;
-// 		for (var i = 0; i < images.length; i++) {
-// 			var farmID = images[i].farm;
-// 			var serverID = images[i].server;
-// 			var id = images[i].id;
-// 			var secret = images[i].secret;
-// 			var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
-// 			landmarkPictures.push(url);
-// 			// console.log('add Images');
-// 		}
-// 	}).fail(function() {
-// 		clearTimeout(flickrRequestTimeout);
-// 	});
-// } 
-
-var ViewModel = function(map, landmarks) {
-	var self = this;
-
-	this.googleMap = map;
-	this.markers = [];
-	this.landmarkList = ko.observableArray([]);
-
-	// Adds Landmark site names to list in DOM
-	landmarks.forEach(function(lmarks, position) {
-		self.landmarkList.push(new Landmarks(lmarks, position));
-	});
-	// console.log(landmarks);
-
-	this.landmarkPictures = ko.observableArray([]);
-	 	// self.landmarkPictures(loadFlickr());	
-	
-	this.loadFlickr = function(location) {
-		var $flickrElem = $('#flickr-images');
-
-		// Clear out old data before new request
-		$flickrElem.text('');
-	 self.landmarkPictures([]);
+	// Clear out old data before new request
+	$flickrElem.text('');
+	viewModel.landmarkPictures([]);
 
 	var key = '88e693240daad97978bc4d93c370fd18';
 	var secret = '78ab07255ac49f98';
@@ -118,30 +72,44 @@ var ViewModel = function(map, landmarks) {
 
 	var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
 
-		$.getJSON(flickrURl, function (data) {
-			console.log(data);
-			var images = data.photos.photo;
-			for (var i = 0; i < images.length; i++) {
-				var farmID = images[i].farm;
-				var serverID = images[i].server;
-				var id = images[i].id;
-				var secret = images[i].secret;
-				var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
-				self.landmarkPictures.push(url);
-				console.log('add Images');
-			}
-		}).fail(function() {
-			clearTimeout(flickrRequestTimeout);
-		});
-	};
+	$.getJSON(flickrURl, function (data) {
+		// console.log(data);
+		var images = data.photos.photo;
+		for (var i = 0; i < images.length; i++) {
+			var farmID = images[i].farm;
+			var serverID = images[i].server;
+			var id = images[i].id;
+			var secret = images[i].secret;
+			var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
+			viewModel.landmarkPictures.push(url);
+			// console.log('add Images');
+		}
+	}).fail(function() {
+		clearTimeout(flickrRequestTimeout);
+	});
+} 
+
+var ViewModel = function(map, landmarks) {
+	var self = this;
+
+	this.googleMap = map;
+	this.markers = [];
+	this.landmarkList = ko.observableArray([]);
+	this.landmarkPictures = ko.observableArray([]);
+
+	// Adds Landmark site names to list in DOM
+	landmarks.forEach(function(lmarks, position) {
+		self.landmarkList.push(new Landmarks(lmarks, position));
+	});
+	// console.log(landmarks);	
 
 	this.listClick = function(clickedLandmark) {
-		// console.log(clickedLandmark);
+		console.log(clickedLandmark);
 		map.setCenter(clickedLandmark.latlngLoc);
 		populateInfoWindow(clickedLandmark.marker);
 		toggleBounce(clickedLandmark.marker);
 		bounceTimer(clickedLandmark.marker);
-		self.loadFlickr(clickedLandmark.name);
+		loadFlickr(clickedLandmark.name);
 	};
 
 	// Knockout Observable for Filtering
