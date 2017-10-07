8bfc7368bdebeeff06c36770713646dd45d25fc9
Samantha Canedy <samcanedy@gmail.com>
2017-10-07 11:03:18 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,67 +2,6 @@
 // and third-party API.
 // The View is the HTML file.
 
-var styles = [
-	{ featureType: 'water',
-		stylers: [{ color: '#19a0d8' }]},
-   	{ featureType: 'administrative', elementType: 'labels.text.stroke',
-        stylers: [{ color: '#ffffff' }, { weight: 6 }]},
-    {featureType: 'administrative', elementType: 'labels.text.fill',
-        stylers: [{ color: '#e85113' }]},
-    {featureType: 'road.highway', elementType: 'geometry.stroke',
-        stylers: [{ color: '#efe9e4' }, { lightness: -40 }]},
-    {featureType: 'transit.station',
-        stylers: [{ weight: 9 }, { hue: '#e85113' }]},
-    {featureType: 'road.highway',elementType: 'labels.icon',
-        stylers: [{ visibility: 'off' }]},
-    {featureType: 'water', elementType: 'labels.text.stroke',
-        stylers: [{ lightness: 100 }]},
-    {featureType: 'water', elementType: 'labels.text.fill',
-        stylers: [{ lightness: -100 }]},
-    {featureType: 'poi', elementType: 'geometry',
-        stylers: [{ visibility: 'on' }, { color: '#f0e4d3' }]},
-    {featureType: 'road.highway', elementType: 'geometry.fill',
-        stylers: [ { color: '#efe9e4' }, { lightness: -25 }]}
-];
-
-var googleMap;
-var infoWindow;
-var markers = [];
-var landmarks = [
-	{
-		name: 'Texas State Capitol',
-		latlngLoc: {lat: 30.2741853, lng: -97.7405401},
-	},
-	{
-		name: 'University of Texas Tower',
-		latlngLoc: {lat: 30.2861062, lng: -97.7393634},
-	},
-	{
-		name: 'Mount Bonnell',
-		latlngLoc: {lat: 30.3207616, lng: -97.77334019999999},
-	},
-	{
-		name: 'Congress Avenue Bridge',
-		latlngLoc: {lat: 30.2617381, lng: -97.74516319999999},
-	},
-	{
-		name: 'Barton Springs',
-		latlngLoc: {lat: 30.2660703, lng: -97.76913479999999},
-	},
-	{
-		name: '360 Bridge / Pennybacker Bridge',
-		latlngLoc: {lat: 30.3516437, lng: -97.79701449999999},
-	},
-	{
-		name: 'Auditorium Shores',
-		latlngLoc: {lat: 30.2627167, lng: -97.7515303},
-	},
-	{
-		name: 'Zilker Park',
-		latlngLoc: {lat: 30.2669624, lng: -97.77285930000001},
-	}
-];
-
 function Landmarks(data, position) {
 	this.name = data.name;
 	this.latlngLoc = data.latlngLoc;
@@ -80,38 +19,22 @@ function Landmarks(data, position) {
 		populateInfoWindow(this);
 		toggleBounce(this);
 		bounceTimer(this);
-		// console.log(marker);
+		// loadFlickr(this.title);
+		// console.log(this);
 	});
 }
 
 function populateInfoWindow(marker) {
-	console.log('populateInfoWindow');
-	infoWindow.marker != marker;
-	infoWindow.setContent('<div>' + marker.title + '</div>');
-	infoWindow.open(googleMap, marker);
-	// Make sure the marker property is cleared if window is closed
-	infoWindow.addListener('closeclick', function() {
-		infoWindow.marker = null;
-	});
+	// console.log('populateInfoWindow');
+	if (infoWindow.marker != marker) {
+		infoWindow.setContent('<div>' + marker.title + '</div>');
+		infoWindow.open(googleMap, marker);
+		// Make sure the marker property is cleared if window is closed
+		infoWindow.addListener('closeclick', function() {
+			infoWindow.marker = null;
+		});
+	}
 	//console.log(markers);
-
-	// var key = '88e693240daad97978bc4d93c370fd18';
-	// var secret = '78ab07255ac49f98';
-
-	// var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
-
-	// $.getJSON(flickrURl, function(marker) {
-	// 	console.log(marker);
-	// 	var images = marker.photos.photo
-	// 	for (var i = 0; i < images.length; i++) {
-	// 		var  farmID = images[i].farm;
-	// 		var serverID = images[i].server;
-	// 		var id = images[i].id;
-	// 		var secret = images[i].secret;
-	// 		var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
-	// 		console.log('add images');
-	// 	}
-	// })
 }
 
 function toggleBounce(marker) {
@@ -123,10 +46,45 @@ function toggleBounce(marker) {
 }
 
 function bounceTimer (marker) {
-	setTimeout(function() {
+	setInterval(function() {
 		marker.setAnimation(null);
-	}, 5000);
+	}, 1400);
 }
+
+// function loadFlickr (landmarks) {
+// 	var $flickrElem = $('#flickr-images');
+
+// 	// this.landmarkPictures = ko.observableArray([]);
+
+// 	// Clear out old data before new request
+// 	$flickrElem.text('');
+// 	landmarkPictures([]);
+
+// 	var key = '88e693240daad97978bc4d93c370fd18';
+// 	var secret = '78ab07255ac49f98';
+
+// 	var flickrRequestTimeout = setInterval(function() {
+// 		$flickrElem.text('Failed to load Flickr Images');
+// 	}, 9000);
+
+// 	var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
+
+// 	$.getJSON(flickrURl, function (data) {
+// 		console.log(data);
+// 		var images = data.photos.photo;
+// 		for (var i = 0; i < images.length; i++) {
+// 			var farmID = images[i].farm;
+// 			var serverID = images[i].server;
+// 			var id = images[i].id;
+// 			var secret = images[i].secret;
+// 			var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
+// 			landmarkPictures.push(url);
+// 			// console.log('add Images');
+// 		}
+// 	}).fail(function() {
+// 		clearTimeout(flickrRequestTimeout);
+// 	});
+// } 
 
 var ViewModel = function(map, landmarks) {
 	var self = this;
@@ -134,7 +92,6 @@ var ViewModel = function(map, landmarks) {
 	this.googleMap = map;
 	this.markers = [];
 	this.landmarkList = ko.observableArray([]);
-	this.loadFlickr = ko.observableArray([]);
 
 	// Adds Landmark site names to list in DOM
 	landmarks.forEach(function(lmarks, position) {
@@ -142,26 +99,24 @@ var ViewModel = function(map, landmarks) {
 	});
 	// console.log(landmarks);
 
-	// Add Flickr API to List View
-	landmarks.forEach(function () {
-		var $flickrHeader = $('#flickr-header');
+	this.landmarkPictures = ko.observableArray([]);
+	 	// self.landmarkPictures(loadFlickr());	
+	
+	this.loadFlickr = function(location) {
 		var $flickrElem = $('#flickr-images');
 
 		// Clear out old data before new request
 		$flickrElem.text('');
+	 self.landmarkPictures([]);
 
-		var key = '88e693240daad97978bc4d93c370fd18';
-		var secret = '78ab07255ac49f98';
+	var key = '88e693240daad97978bc4d93c370fd18';
+	var secret = '78ab07255ac49f98';
 
-		var flickrRequestTimeout = setTimeout(function() {
-			$flickrElem.text('Failed to load Flickr Images');
-		}, 9000);
+	var flickrRequestTimeout = setInterval(function() {
+		$flickrElem.text('Failed to load Flickr Images');
+	}, 9000);
 
-		var landmark = $('#search-text');
-	    var location = landmark.val();
-
-		var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
-		this.url = ko.observable('');
+	var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
 
 		$.getJSON(flickrURl, function (data) {
 			console.log(data);
@@ -171,54 +126,22 @@ var ViewModel = function(map, landmarks) {
 				var serverID = images[i].server;
 				var id = images[i].id;
 				var secret = images[i].secret;
-				self.url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
-				self.loadFlickr.push( {url: self.url} );  // '<li class=image><img src="' + url + '""></li>');
+				var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
+				self.landmarkPictures.push(url);
 				console.log('add Images');
 			}
+		}).fail(function() {
+			clearTimeout(flickrRequestTimeout);
 		});
-		clearTimeout(flickrRequestTimeout);
-	})
-	
-	// this.loadFlickr = function() {
-	// 	var $flickrHeader = $('#flickr-header');
-	// 	var $flickrElem = $('#flickr-images');
-
-	// 	// Clear out old data before new request
-	// 	$flickrElem.text('');
-
-	// 	var key = '88e693240daad97978bc4d93c370fd18';
-	// 	var secret = '78ab07255ac49f98';
-
-	// 		var flickrRequestTimeout = setTimeout(function() {
-	// 		$flickrElem.text('Failed to load Flickr Images');
-	// 	}, 9000);
-
-	// 	var landmark = $('#search-text');
-	//     var location = landmark.val();
-
-	// 	var flickrURl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + key + '&text=' + location + '&format=json&nojsoncallback=1';
-
-	// 	$.getJSON(flickrURl, function (data) {
-	// 		console.log(data);
-	// 		var images = data.photos.photo;
-	// 		for (var i = 0; i < images.length; i++) {
-	// 			var farmID = images[i].farm;
-	// 			var serverID = images[i].server;
-	// 			var id = images[i].id;
-	// 			var secret = images[i].secret;
-	// 			var url = 'https://farm' + farmID + '.staticflickr.com/' + serverID + '/' + id + '_' + secret+ '.jpg';
-	// 			$flickrElem.append('<li class=image><img src="' + url + '""></li>');
-	// 			console.log('add Images');
-	// 		}
-	// 	});
-	// 	clearTimeout(flickrRequestTimeout);
-	// };
+	};
 
 	this.listClick = function(clickedLandmark) {
-		console.log(clickedLandmark);
+		// console.log(clickedLandmark);
+		map.setCenter(clickedLandmark.latlngLoc);
 		populateInfoWindow(clickedLandmark.marker);
 		toggleBounce(clickedLandmark.marker);
 		bounceTimer(clickedLandmark.marker);
+		self.loadFlickr(clickedLandmark.name);
 	};
 
 	// Knockout Observable for Filtering
@@ -227,28 +150,12 @@ var ViewModel = function(map, landmarks) {
 	// Adds Filtering Functionality
 	this.filteredLandmark = ko.computed(function() {
 		var filter = self.filteredText().toLowerCase();
-		if(!filter) {
-			// If there is not a filter, then return the whole list and markers.
-			return self.landmarkList();
-			// Add default to show all markers.
-		} else {
-			// console.log('Filtering');
 			return ko.utils.arrayFilter(self.landmarkList(), function(filteredMarker) {
 				// console.log(filteredMarker);
 				var match = filteredMarker.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
 				filteredMarker.marker.setVisible(match);
 				return match;
-
-				// // Initial attempt at filtering. Trouble with the stringStartsWith.
-				// var stringStartsWith = function(string, startsWith) {
-				// 	string = string || '';
-				// 	if(startsWith.length > string.length)
-				// 		return false;
-				// 	return string.substring(0, startsWith.length) === startsWith;
-				// };
-				// return stringStartsWith(lmarks.name.toLowerCase().filter);
 			});
-		}
 	});
 };
 
